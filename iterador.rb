3eb5bf2a6ec042b086ac70c0b710c62b476ce2bc
xhispams <locopor.m3ry@gmail.com>
2020-10-22 05:08:08 -0300

    sending the iterador.rb  challenge to the repository

---
+++ iterador.rb
@@ -0,0 +1,13 @@
+#@Author = Álvaro Moya Santana
+=begin
+Código antiguo que se debe mejorar
+i=0
+while i < 50
+  puts "Iteración #{i}"
+i=i+1 
+=end
+
+50.times do |i|
+puts "Iteración #{i}"
+i=i+1
+end
